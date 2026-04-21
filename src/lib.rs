5f23a99804e4c3dcd7450dd6b497f3f2969e44b8
Carl Hurd <carl@basilisklabs.com>
2026-04-21 19:49:52 -0400

    Add multiple tools

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,8 @@ use extism_pdk::*;
 pub fn greet(name: String) -> FnResult<String> {
     Ok(format!("Hello, {} from Wasm!", name))
 }
+
+#[plugin_fn]
+pub fn shout(input: String) -> FnResult<String> {
+    Ok(input.to_uppercase())
+}
