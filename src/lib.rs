95397d93c30dc24cb1ec97e18f848f43f1db7583
Carl Hurd <carl@basilisklabs.com>
2026-04-23 12:22:29 -0400

    Add judge tool for testing

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,11 @@ use extism_pdk::*;
 use wasm_forge_pdk::forge_types;
 
 forge_types! {
+    pub struct JudgeArgs {
+        /// Is judgement allowed
+        pub allowed: bool,
+    }
+
     pub struct GreetArgs {
         /// The name of the person to greet.
         pub name: String,
@@ -10,6 +15,15 @@ forge_types! {
     pub struct ShoutArgs {
         /// The message to convert to uppercase.
         pub message: String,
+    }
+}
+
+#[plugin_fn]
+pub fn judge(Json(args): Json<JudgeArgs>) -> FnResult<String> {
+    if args.allowed {
+        Ok("I am fit to judge anyone.".to_string())
+    } else {
+        Ok("I am unfit to judge anyone.".to_string())
     }
 }
 
